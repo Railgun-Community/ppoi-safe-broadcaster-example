12f3caac6b2ff7947051f925d57906805ce11a67
Chris Troutner <chris.troutner@gmail.com>
2021-09-30 09:24:30 -0700

    Scaffolded hello world

---
+++ relayer/relayer.js
@@ -0,0 +1,12 @@
+/*
+  Top level file for starting the Relayer daemon.
+*/
+
+async function startRelayer() {
+  try {
+    console.log('hello world');
+  } catch (err) {
+    console.error(err);
+  }
+}
+startRelayer();
