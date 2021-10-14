acef9e67788be836cba45b5624512db920dd8730
Chris Troutner <chris.troutner@gmail.com>
2021-10-14 08:09:44 -0700

    Added 100% test coverage for railgun-rate command

--- cli-wallet/src/commands/railgun-rate.js
+++ cli-wallet/src/commands/railgun-rate.js
@@ -3,50 +3,50 @@
 */
 
 // Public NPM libraries
-const Conf = require('conf');
-const { Command } = require('@oclif/command');
+const Conf = require('conf')
+const { Command } = require('@oclif/command')
 
 // Local libraries
-const RelayerService = require('../lib/adapters/relayer-service');
+const RelayerService = require('../lib/adapters/relayer-service')
 
 class Rate extends Command {
-  constructor(argv, config) {
-    super(argv, config);
+  constructor (argv, config) {
+    super(argv, config)
 
     // Encapsulate dependencies.
-    this.relayerService = new RelayerService();
-    this.conf = new Conf();
+    this.relayerService = new RelayerService()
+    this.conf = new Conf()
   }
 
-  async run() {
+  async run () {
     try {
       // const { flags } = this.parse(About);
 
-      const data = await this.getRate();
-      console.log('Relayer rate: ', data);
+      const data = await this.getRate()
+      console.log('Relayer rate: ', data)
     } catch (err) {
-      console.log('Error in railgun-rate.js/run(): ', err);
+      console.log('Error in railgun-rate.js/run(): ', err)
 
-      return 0;
+      return 0
     }
   }
 
   // Query the selected Relayer for information about its rate.
-  async getRate() {
+  async getRate () {
     try {
-      const result = await this.relayerService.getRate();
+      const result = await this.relayerService.getRate()
       // console.log('data: ', result);
 
-      return result.message;
+      return result.message
     } catch (err) {
-      console.error('Error in getRate()');
-      throw err;
+      console.error('Error in getRate()')
+      throw err
     }
   }
 }
 
-Rate.description = 'Query the /rate RPC endpoint of the selected Railgun Relay.';
+Rate.description = 'Query the /rate RPC endpoint of the selected Railgun Relay.'
 
-Rate.flags = {};
+Rate.flags = {}
 
-module.exports = Rate;
+module.exports = Rate
