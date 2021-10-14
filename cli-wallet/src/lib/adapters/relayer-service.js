20e867f71e8c50e400b0936e00af39c1a523ad8b
Chris Troutner <chris.troutner@gmail.com>
2021-10-14 07:27:07 -0700

    Added rate command

--- cli-wallet/src/lib/adapters/relayer-service.js
+++ cli-wallet/src/lib/adapters/relayer-service.js
@@ -4,108 +4,78 @@
 */
 
 // Configuration variables.
-const LOCAL_REST_API = 'http://localhost:5000/p2wdb/'
+const LOCAL_REST_API = 'http://localhost:5000/relayer/';
 
 // Public npm libraries.
-const axios = require('axios')
-const Conf = require('conf')
+const axios = require('axios');
+const Conf = require('conf');
 
 class RelayerService {
-  constructor (localConfig = {}) {
+  constructor(localConfig = {}) {
     // Encapsulate dependencies
-    this.axios = axios
-    this.conf = new Conf()
+    this.axios = axios;
+    this.conf = new Conf();
   }
 
-  checkServiceId () {
+  checkServiceId() {
     // this.conf = new Conf()
 
-    const serviceId = this.conf.get('selectedService')
+    const serviceId = this.conf.get('selectedService');
 
     if (!serviceId) {
-      throw new Error('Relayer service ID does not exist in config.')
+      throw new Error('Relayer service ID does not exist in config.');
     }
 
-    return serviceId
+    return serviceId;
   }
 
   // Query the /about RPC endpoint.
-  async getAbout () {
+  async getAbout() {
     try {
-      const serviceId = this.checkServiceId()
-
-      const result = await this.axios.post(LOCAL_REST_API, {
-        sendTo: serviceId,
-        rpcData: {}
-      })
-      // console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`)
-
-      // If there is a timeout or other network failure.
-      if (result.data.success === false) throw new Error(result.data.message)
-
-      return result.data
-    } catch (err) {
-      console.error('Error in getAbout()')
-      throw err
-    }
-  }
-
-  // Read an entry from the P2WDB, given an entry hash.
-  async getEntry (hash) {
-    try {
-      // Input validation.
-      if (!hash || typeof hash !== 'string') {
-        throw new Error('getEntry() input hash must be a string.')
-      }
-
-      const serviceId = this.checkServiceId()
+      const serviceId = this.checkServiceId();
 
       const result = await this.axios.post(LOCAL_REST_API, {
         sendTo: serviceId,
         rpcData: {
-          endpoint: 'getByHash',
-          hash
-        }
-      })
+          method: 'about',
+          params: {},
+        },
+      });
       // console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`)
 
       // If there is a timeout or other network failure.
-      if (result.data.success === false) throw new Error(result.data.message)
+      if (result.data.success === false) throw new Error(result.data.message);
 
-      return result.data
+      return result.data;
     } catch (err) {
-      console.error('Error in getEntry()')
-      throw err
+      console.error('Error in getAbout()');
+      throw err;
     }
   }
 
-  // Write an entry to the P2WDB
-  async writeEntry (data) {
+  // Query the /about RPC endpoint.
+  async getRate() {
     try {
-      const serviceId = this.checkServiceId()
+      const serviceId = this.checkServiceId();
 
       const result = await this.axios.post(LOCAL_REST_API, {
         sendTo: serviceId,
         rpcData: {
-          endpoint: 'write',
-          txid: data.txid,
-          message: data.message,
-          signature: data.signature,
-          data: data.data
-        }
-      })
+          method: 'rate',
+          params: {},
+        },
+      });
       // console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`)
 
       // If there is a timeout or other network failure.
-      if (result.data.success === false) throw new Error(result.data.message)
+      if (result.data.success === false) throw new Error(result.data.message);
 
-      // The hash of the new entry.
-      return result.data.data
+      return result.data;
     } catch (err) {
-      console.error('Error in writeEntry()')
-      throw err
+      console.error('Error in getAbout()');
+      throw err;
     }
   }
 }
 
-module.exports = RelayerService
+module.exports = RelayerService;
