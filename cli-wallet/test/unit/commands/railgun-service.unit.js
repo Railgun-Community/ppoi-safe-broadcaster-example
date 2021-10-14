ace3a28883b14001cff2a2c9736992f86d398c6f
Chris Troutner <chris.troutner@gmail.com>
2021-10-13 19:58:32 -0700

    Close to 100% test coverage of railgun-service.js command

--- cli-wallet/test/unit/commands/railgun-service.unit.js
+++ cli-wallet/test/unit/commands/railgun-service.unit.js
@@ -3,68 +3,130 @@
 */
 
 // Public npm packages.
-const { assert } = require('chai');
-const sinon = require('sinon');
+const { assert } = require('chai')
+const sinon = require('sinon')
 
 // Local libraries
-const RailgunService = require('../../../src/commands/railgun-service');
+const RailgunService = require('../../../src/commands/railgun-service')
 // const RelaysMock = require('../../mocks/ipfs-relays-mock');
 
 describe('#railgun-about', () => {
-  let sandbox;
-  let uut;
+  let sandbox
+  let uut
 
   beforeEach(async () => {
-    sandbox = sinon.createSandbox();
+    sandbox = sinon.createSandbox()
 
-    uut = new RailgunService();
-  });
+    uut = new RailgunService()
+  })
 
   afterEach(() => {
-    sandbox.restore();
-  });
+    sandbox.restore()
+  })
 
   describe('#selectService', () => {
     it('should select a service and return true', () => {
       // Mock dependencies so real config is not changed.
-      sandbox.stub(uut.conf, 'set').returns();
+      sandbox.stub(uut.conf, 'set').returns()
 
       const flags = {
-        select: 'QmcewynF2DMxuvK7zk1E5es1cvBwZrfnYEaiN995KVYaKp',
-      };
+        select: 'QmcewynF2DMxuvK7zk1E5es1cvBwZrfnYEaiN995KVYaKp'
+      }
 
-      const result = uut.selectService(mockPeers, flags);
+      const result = uut.selectService(mockPeers, flags)
       // console.log('result: ', result);
 
-      assert.equal(result, true);
-    });
+      assert.equal(result, true)
+    })
 
     it('should return false if service can not be found', () => {
       // Mock dependencies so real config is not changed.
-      sandbox.stub(uut.conf, 'set').returns();
+      sandbox.stub(uut.conf, 'set').returns()
 
       const flags = {
-        select: 'QmcewynF2DMxuvK7zk1E5es1cvBwZrfnYEaiN995KVYaKo',
-      };
+        select: 'QmcewynF2DMxuvK7zk1E5es1cvBwZrfnYEaiN995KVYaKo'
+      }
 
-      const result = uut.selectService(mockPeers, flags);
+      const result = uut.selectService(mockPeers, flags)
       // console.log('result: ', result);
 
-      assert.equal(result, false);
-    });
+      assert.equal(result, false)
+    })
 
     it('should catch and throw errors', () => {
       try {
-        uut.selectService();
+        uut.selectService()
 
-        assert.fail('Unexpected code path');
+        assert.fail('Unexpected code path')
       } catch (err) {
         // console.log(err);
-        assert.include(err.message, 'Cannot read property');
+        assert.include(err.message, 'Cannot read property')
       }
-    });
-  });
-});
+    })
+  })
+
+  describe('#getServicePeers', () => {
+    it('should return all the service peers', async () => {
+      // Mock dependencies
+      sandbox.stub(uut.axios, 'post').resolves({ data: mockPeers })
+      const flags = {}
+
+      const result = await uut.getServicePeers(flags)
+      // console.log('result: ', result);
+
+      // Result should be an array with one element.
+      assert.isArray(result)
+      assert.equal(result.length, 1)
+
+      // Property should have expected properties.
+      assert.property(result[0], 'addr')
+      assert.property(result[0], 'peer')
+      assert.property(result[0], 'direction')
+      assert.property(result[0], 'name')
+      assert.property(result[0], 'protocol')
+      assert.property(result[0], 'version')
+      assert.property(result[0], 'isSelected')
+    })
+
+    it('should catch, report, and throw errors', async () => {
+      try {
+        // Force an error
+        sandbox.stub(uut.axios, 'post').rejects(new Error('test error'))
+
+        await uut.getServicePeers()
+
+        assert.fail('Unexpected code path')
+      } catch (err) {
+        assert.include(err.message, 'test error')
+      }
+    })
+
+    // TODO:
+    // it('should handle peers without a protocol specified')
+
+    // TODO:
+    // it('should set a new service peer when select flag is present')
+  })
+
+  describe('#run', () => {
+    it('should report service peers and return true', async () => {
+      // Mock dependencies
+      sandbox.stub(uut, 'parse').returns({ flags: {} })
+      sandbox.stub(uut, 'getServicePeers').resolves(mockPeers[0])
+
+      const result = await uut.run()
+      // console.log('result: ', result);
+
+      assert.equal(result, true)
+    })
+
+    it('should report errors and return false', async () => {
+      const result = await uut.run()
+
+      assert.equal(result, false)
+    })
+  })
+})
 
 const mockPeers = [
   {
@@ -73,7 +135,7 @@ const mockPeers = [
     direction: 'outbound',
     name: 'trout-dev-railgun-relay',
     protocol: 'railgun-relayer',
-    version: '1.0.1',
+    version: '1.0.1'
   },
   {
     addr: '/ip4/139.162.76.54/tcp/5269/ws/p2p/QmaKzQTAtoJWYMiG5ATx41uWsMajr1kSxRdtg919s8fK77',
@@ -81,6 +143,6 @@ const mockPeers = [
     direction: 'outbound',
     name: 'ipfs-relay-tokyo-pfs-0945772',
     protocol: 'generic-service',
-    version: '1.3.0',
-  },
-];
+    version: '1.3.0'
+  }
+]
