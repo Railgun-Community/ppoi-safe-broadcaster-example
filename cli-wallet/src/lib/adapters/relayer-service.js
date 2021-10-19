283074cbffbff2b1c8cc9c7a8d3123d1fb3a9af6
Chris Troutner <chris.troutner@gmail.com>
2021-10-19 12:35:30 -0700

    Added relayer-submit-tx command with 100% test coverage

--- cli-wallet/src/lib/adapters/relayer-service.js
+++ cli-wallet/src/lib/adapters/relayer-service.js
@@ -4,98 +4,101 @@
 */
 
 // Configuration variables.
-const LOCAL_REST_API = 'http://localhost:5000/relayer/';
+const LOCAL_REST_API = 'http://localhost:5000/relayer/'
 
 // Public npm libraries.
-const axios = require('axios');
-const Conf = require('conf');
+const axios = require('axios')
+const Conf = require('conf')
 
 class RelayerService {
-  constructor(localConfig = {}) {
+  constructor (localConfig = {}) {
     // Encapsulate dependencies
-    this.axios = axios;
-    this.conf = new Conf();
+    this.axios = axios
+    this.conf = new Conf()
   }
 
-  checkServiceId() {
+  checkServiceId () {
     // this.conf = new Conf()
 
-    const serviceId = this.conf.get('selectedService');
+    const serviceId = this.conf.get('selectedService')
 
     if (!serviceId) {
-      throw new Error('Relayer service ID does not exist in config.');
+      throw new Error('Relayer service ID does not exist in config.')
     }
 
-    return serviceId;
+    return serviceId
   }
 
   // Query the /about RPC endpoint.
-  async getAbout() {
+  async getAbout () {
     try {
-      const serviceId = this.checkServiceId();
+      const serviceId = this.checkServiceId()
 
       const result = await this.axios.post(LOCAL_REST_API, {
         sendTo: serviceId,
         rpcData: {
           method: 'about',
-          params: {},
-        },
-      });
+          params: {}
+        }
+      })
       // console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`)
 
       // If there is a timeout or other network failure.
-      if (result.data.success === false) throw new Error(result.data.message);
+      if (result.data.success === false) throw new Error(result.data.message)
 
-      return result.data;
+      return result.data
     } catch (err) {
-      console.error('Error in getAbout()');
-      throw err;
+      console.error('Error in getAbout()')
+      throw err
     }
   }
 
-  // Query the /about RPC endpoint.
-  async getRate() {
+  // Get the Rate to be paid to the Relayer for relaying the transaction.
+  async getRate () {
     try {
-      const serviceId = this.checkServiceId();
+      const serviceId = this.checkServiceId()
 
       const result = await this.axios.post(LOCAL_REST_API, {
         sendTo: serviceId,
         rpcData: {
           method: 'rate',
-          params: {},
-        },
-      });
-      console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`);
+          params: {}
+        }
+      })
+      console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`)
 
       // If there is a timeout or other network failure.
-      if (result.data.success === false) throw new Error(result.data.message);
+      if (result.data.success === false) throw new Error(result.data.message)
 
-      return result.data;
+      return result.data
     } catch (err) {
-      console.error('Error in getRate()');
-      throw err;
+      console.error('Error in getRate()')
+      throw err
     }
   }
 
-  async submitTx(hex) {
+  // Submit the TX hex to the Relayer.
+  async submitTx (hex) {
     try {
-      const serviceId = this.checkServiceId();
+      const serviceId = this.checkServiceId()
 
       const result = await this.axios.post(LOCAL_REST_API, {
         sendTo: serviceId,
         rpcData: {
           method: 'submitTx',
           params: {
-            hex,
-          },
-        },
-      });
-      console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`);
+            hex
+          }
+        }
+      })
+      // console.log(`result.data: ${JSON.stringify(result.data, null, 2)}`);
+
+      return result.data
     } catch (err) {
-      console.error('Error in submitTx()');
-      throw err;
+      console.error('Error in submitTx()')
+      throw err
     }
   }
 }
 
-module.exports = RelayerService;
+module.exports = RelayerService
