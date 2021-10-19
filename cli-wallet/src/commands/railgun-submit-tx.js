da05f7baa3fe13e1fcbe1f77df24c6c47ec80ca7
Chris Troutner <chris.troutner@gmail.com>
2021-10-19 13:08:27 -0700

    Added 100% unit test coverage for submit-tx

--- cli-wallet/src/commands/railgun-submit-tx.js
+++ cli-wallet/src/commands/railgun-submit-tx.js
@@ -3,52 +3,52 @@
 */
 
 // Public NPM libraries
-const Conf = require('conf')
-const { Command, flags } = require('@oclif/command')
+const Conf = require('conf');
+const { Command, flags } = require('@oclif/command');
 
 // Local libraries
-const RelayerService = require('../lib/adapters/relayer-service')
+const RelayerService = require('../lib/adapters/relayer-service');
 
 class SubmitTx extends Command {
-  constructor (argv, config) {
-    super(argv, config)
+  constructor(argv, config) {
+    super(argv, config);
 
     // Encapsulate dependencies.
-    this.relayerService = new RelayerService()
-    this.conf = new Conf()
+    this.relayerService = new RelayerService();
+    this.conf = new Conf();
   }
 
-  async run () {
+  async run() {
     try {
-      const { flags } = this.parse(SubmitTx)
+      const { flags } = this.parse(SubmitTx);
 
-      const data = await this.submitTx(flags)
-      console.log('Relayer message: ', data)
+      const data = await this.submitTx(flags);
+      console.log('Relayer message: ', data);
     } catch (err) {
-      console.log('Error in railgun-submit-tx.js/run(): ', err)
+      console.log('Error in railgun-submit-tx.js/run(): ', err);
 
-      return 0
+      return 0;
     }
   }
 
   // Query the selected Relayer for information about its rate.
-  async submitTx (flags) {
+  async submitTx(flags) {
     try {
-      const result = await this.relayerService.submitTx(flags.hex)
+      const result = await this.relayerService.submitTx(flags.hex);
       // console.log('result: ', result);
 
-      return result.message
+      return result.message;
     } catch (err) {
-      console.error('Error in submitTx()')
-      throw err
+      console.error('Error in submitTx()');
+      throw err;
     }
   }
 }
 
-SubmitTx.description = 'Send a TX to the Relayer, to be broadcast on behalf this wallet.'
+SubmitTx.description = 'Send a TX to the Relayer, to be broadcast on behalf this wallet.';
 
 SubmitTx.flags = {
-  hex: flags.string({ char: 'e', description: 'Hex string of a tx to send to Relayer' })
-}
+  hex: flags.string({ char: 'h', description: 'Hex string of a tx to send to Relayer' }),
+};
 
-module.exports = SubmitTx
+module.exports = SubmitTx;
--- relayer/test/unit/controllers/submit-tx.json-rpc.controller.unit.js
+++ relayer/test/unit/controllers/submit-tx.json-rpc.controller.unit.js
@@ -22,23 +22,30 @@ describe("#SubmitTxRPC", () => {
   afterEach(() => sandbox.restore());
 
   describe("#submitTxRouter", () => {
-    // it('should acknowledge reciept of a tx', async () => {
-    //   const rpcData =
-    //
-    //   const result = await uut.submitTxRouter(rpcData)
-    // })
-    //
-    // it("should return rate information", async () => {
-    //   const result = await uut.rateRouter();
-    //   // console.log("result: ", result);
-    //
-    //   assert.property(result, "success");
-    //   assert.equal(result.success, true);
-    //   assert.property(result, "status");
-    //   assert.equal(result.status, 200);
-    //   assert.property(result, "message");
-    //   assert.property(result, "endpoint");
-    //   assert.equal(result.endpoint, "rate");
-    // });
+    it("should acknowledge reciept of a tx", async () => {
+      const result = await uut.submitTxRouter(mockRpcData);
+      // console.log("result: ", result);
+
+      // Assert expected properties and values exist.
+      assert.property(result, "success");
+      assert.equal(result.success, true);
+      assert.property(result, "status");
+      assert.equal(result.status, 200);
+      assert.property(result, "message");
+      assert.property(result, "hex");
+      assert.property(result, "endpoint");
+      assert.equal(result.endpoint, "submitTx");
+    });
   });
 });
+
+const mockRpcData = {
+  payload: {
+    jsonrpc: "2.0",
+    id: "46ebe70e-4b95-4c11-abc2-a4a9edc56c2b",
+    method: "submitTx",
+    params: { hex: "test" }
+  },
+  type: "request",
+  from: "QmUpc6JVSiCrm4gZkQGDTTKn9g5uGzc25HAEs9qqShw8Rs"
+};
