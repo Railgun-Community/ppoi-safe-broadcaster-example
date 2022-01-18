513e13435beee40270d185d1473bd97e199cc25f
Dylan Oliver <dylan.oliver@gmail.com>
2022-01-18 11:50:37 -0500

    wipe repo

--- config/env/production.js
+++
@@ -1,9 +0,0 @@
-/*
-  These are the environment settings for the PRODUCTION environment.
-  This is the environment run with `npm start` if RELAY_ENV=production.
-  This is the environment run inside the Docker container.
-*/
-
-module.exports = {
-  env: "prod"
-};
