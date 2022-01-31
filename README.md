41ca57beed309c5cad8c90492a1001330e7be421
jrmeurer <jrmeurer@wisc.edu>
2022-01-31 13:53:52 -0700

    Clean up unused code, update readme

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 relay jsonrpc messages over js-waku (https://wakuconnect.dev/)
 
-- install pnpm, or just use npm/yarn
+# Run code
+
+- install pnpm
 - `pnpm install`
 - `export DEBUG=delayer:*` to enable logging
 - `pnpm start`
+
+# Run tests
+
+- `pnpm test`
+- `pnpm run test-coverage` (with code coverage visualizer)
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,15 +1,3 @@
-export const hexToBuf = (hex: string | Buffer | Uint8Array): Buffer => {
-  if (typeof hex === 'string') {
-    return Buffer.from(hex.replace(/^0x/i, ''), 'hex');
-  }
-  return Buffer.from(hex);
-};
-
-export const bufToHex = (buf: Uint8Array | Buffer | ArrayBuffer): string => {
-  const _buf = Buffer.from(buf);
-  return _buf.toString('hex');
-};
-
 export const removeNaNs = (a: number[]): number[] => {
   const newArray: number[] = [];
   a.forEach((item) => {
