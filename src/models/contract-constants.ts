fdef73e70927646ca883d9cd3ee5e6749d9defaf
jrmeurer <jrmeurer@wisc.edu>
2022-02-14 08:52:24 -0700

    Update proxy contract ropsten

--- src/models/contract-constants.ts
+++ src/models/contract-constants.ts
@@ -1,6 +1,6 @@
 export enum RailProxyContract {
-  Ethereum = '0xbf0Af567D60318f66460Ec78b464589E3f9dA48e',
-  Ropsten = '0x791532E6155E0F69cEE328B356C8B6A8DaFB9076',
+  Ethereum = '0xbf0Af567D60318f66460Ec78b464589E3f9dA48e', // TODO will need update
+  Ropsten = '0x3eAf99B5EDc79D833AA8B6d18F0a8dd041e13eF6',
   BinanceSmartChain = '0xCe567352AeE08F11682B71D58685eb9b01ea045a', // TODO will need update
   PolygonPOS = '0x61ca7a0346a10cea849910C29617ac316461AD76', // TODO will need update
   HardHat = '0x0165878A594ca255338adfa4d48449f69242Eb8F',
