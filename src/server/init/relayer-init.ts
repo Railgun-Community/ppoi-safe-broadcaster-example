69311b67a6a43ca6c500c5368d5d79faf9ee0ba6
Dylan Oliver <dylan.oliver@gmail.com>
2022-02-15 15:53:17 -0500

    fix path for tests.

--- src/server/init/relayer-init.ts
+++ src/server/init/relayer-init.ts
@@ -1,4 +1,4 @@
-import configDefaults from 'server/config/config-defaults';
+import configDefaults from '../config/config-defaults';
 import { initLepton } from '../lepton/lepton-init';
 import { initWallets } from '../wallets/active-wallets';
 import { initNetworkProviders } from '../providers/active-network-providers';
