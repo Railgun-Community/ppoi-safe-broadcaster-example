bce79210891064096f02a8a249f31ccfbc7c2c0a
jrmeurer <jrmeurer@wisc.edu>
2022-02-18 11:13:16 -0700

    Add env-cmd dep

---
+++ src/server/waku-relayer/__tests__/waku-relayer.test.ts
@@ -0,0 +1,10 @@
+/* globals describe, before, it, beforeEach, afterEach */
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+describe('waku-relayer', () => {
+  before(() => {});
+}).timeout(10000);
