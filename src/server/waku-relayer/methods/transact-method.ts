40d3cf179395c1db6f4af0dd1bf11ae6edf36e76
jrmeurer <jrmeurer@wisc.edu>
2022-02-18 18:48:17 -0700

    Add type for transact params

--- src/server/waku-relayer/methods/transact-method.ts
+++ src/server/waku-relayer/methods/transact-method.ts
@@ -1,15 +1,23 @@
 import { JsonRpcResult } from '@walletconnect/jsonrpc-types';
 import { formatJsonRpcResult } from '@walletconnect/jsonrpc-utils';
 import debug from 'debug';
+import { NetworkChainID } from '../../config/config-chain-ids';
 import { processTransaction } from '../../transactions/process-transaction';
 import { getRailgunWalletPubKey } from '../../wallets/active-wallets';
 
+export type WakuMethodParamsTransact = {
+  serializedTransaction: string;
+  pubkey: string;
+  chainID: NetworkChainID;
+  feesID: string;
+};
+
 export const transactMethod = async (
-  params: any,
+  params: WakuMethodParamsTransact,
   id: number,
   logger: debug.Debugger,
 ): Promise<Optional<JsonRpcResult<string>>> => {
-  const { chainID, feeID, serializedTransaction, pubkey } = params;
+  const { chainID, feesID: feeCacheID, serializedTransaction, pubkey } = params;
   const railgunWalletPubKey = getRailgunWalletPubKey();
   if (railgunWalletPubKey !== pubkey) {
     return undefined;
@@ -17,7 +25,7 @@ export const transactMethod = async (
   try {
     const txResponse = await processTransaction(
       chainID,
-      feeID,
+      feeCacheID,
       serializedTransaction,
     );
     logger('txResponse');
