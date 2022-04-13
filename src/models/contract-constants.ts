0e03f143b1a6c037f4f661d8862d4c1602fa650b
jrmeurer <jrmeurer@wisc.edu>
2022-04-13 10:22:10 -0600

    Update ropsten contract address

--- src/models/contract-constants.ts
+++ src/models/contract-constants.ts
@@ -1,6 +1,6 @@
 export enum RailProxyContract {
   Ethereum = '0xbf0Af567D60318f66460Ec78b464589E3f9dA48e', // TODO will need update
-  Ropsten = '0x3eAf99B5EDc79D833AA8B6d18F0a8dd041e13eF6',
+  Ropsten = '0xDCD05EF739BF992b9818604CE4f860dfaf851b8e', // TODO will need update
   BNBSmartChain = '0xCe567352AeE08F11682B71D58685eb9b01ea045a', // TODO will need update
   PolygonPOS = '0x61ca7a0346a10cea849910C29617ac316461AD76', // TODO will need update
   HardHat = '0x0165878A594ca255338adfa4d48449f69242Eb8F',
