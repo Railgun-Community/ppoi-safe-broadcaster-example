8fd04a5a556bb7a81d2040452ef63625994b1d99
jrmeurer <jrmeurer@wisc.edu>
2022-04-30 17:30:18 -0600

    Try without checking ts types

--- src/server/init/relayer-init.ts
+++ src/server/init/relayer-init.ts
@@ -4,12 +4,13 @@ import { initNetworkProviders } from '../providers/active-network-providers';
 import { initPricePoller, stopPolling } from '../tokens/token-price-poller';
 import { logger } from '../../util/logger';
 import { initSettingsDB, closeSettingsDB } from '../db/settings-db';
+// @ts-ignore
 import { myConfigOverrides } from '../../MY-CONFIG';
 import { initTokens } from '../tokens/network-tokens';
 
 export const initRelayerModules = async (forTest = false) => {
   if (!forTest) {
-    myConfigOverrides();
+    myConfigOverrides && myConfigOverrides();
   }
   initSettingsDB();
   initLepton();
