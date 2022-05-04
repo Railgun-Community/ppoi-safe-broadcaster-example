2e81411d5667b1fcc8ec2f28aea2cedd146249ad
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-04 14:39:12 -0400

    update docker/run-waku.sh

--- docker/run-waku.sh
+++ docker/run-waku.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/wakunode --nat=none --rpc=true --rpc-private=true --rpc-admin=true --relay=true --staticnode=/ip4/167.71.240.30/tcp/60000/p2p/16Uiu2HAmMUjGmiUhJeiZgu6ZZnLRkE2VViR2JgjqtW9aTZnHQqgg --log-level=info --rpc-address=0.0.0.0 --rpc-port=8546
+/usr/bin/wakunode --nat=none --rpc=true --rpc-private=true --rpc-admin=true --relay=true --staticnode=/dns4/relayer.of.holdings/tcp/60000/p2p/16Uiu2HAmMUjGmiUhJeiZgu6ZZnLRkE2VViR2JgjqtW9aTZnHQqgg --log-level=info --rpc-address=0.0.0.0 --rpc-port=8546
