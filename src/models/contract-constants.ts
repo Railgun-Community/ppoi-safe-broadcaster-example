5dac7105ee42b07c78390c432af911c544b63225
jrmeurer <jrmeurer@wisc.edu>
2022-05-09 08:51:03 -0600

    Update deployment blocks

--- src/models/contract-constants.ts
+++ src/models/contract-constants.ts
@@ -1,15 +1,15 @@
 export enum RailProxyContract {
-  Ethereum = '0xbf0Af567D60318f66460Ec78b464589E3f9dA48e', // TODO will need update
+  Ethereum = '0xfa7093cdd9ee6932b4eb2c9e1cde7ce00b1fa4b9',
   Ropsten = '0x1c2A4092e2a436d78FcdF3a178a4E1ed87f0bB8F',
-  BNBSmartChain = '0xCe567352AeE08F11682B71D58685eb9b01ea045a', // TODO will need update
-  PolygonPOS = '0x61ca7a0346a10cea849910C29617ac316461AD76', // TODO will need update
+  BNBSmartChain = '0x590162bf4b50f6576a459b75309ee21d92178a10',
+  PolygonPOS = '0x19b620929f97b7b990801496c3b361ca5def8c71',
   HardHat = '0x0165878A594ca255338adfa4d48449f69242Eb8F',
 }
 
 export enum RailProxyDeploymentBlock {
-  Ethereum = 0, // TODO will need update
+  Ethereum = 14693013,
   Ropsten = 12226000,
-  BNBSmartChain = 0, // TODO will need update
-  PolygonPOS = 0, // TODO will need update
+  BNBSmartChain = 17431925,
+  PolygonPOS = 14693013,
   HardHat = 0,
 }
