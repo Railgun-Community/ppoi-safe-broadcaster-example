fe455c36123b45a17ddd91c765d02a1dc7bc9f59
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-18 16:13:47 -0400

    update docker nwaku with config file, generate config from .env

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-docker build -t relayer:latest -f docker/Dockerfile .
-docker build -t wakunode:latest -f docker/Dockerfile-waku docker
+# docker build -t relayer:latest -f docker/Dockerfile .
+if [ -f docker/.env ]; then
+  echo "found config, good job"
+  export $(cat docker/.env | xargs)
+  envsubst < docker/config.toml.in > docker/config.toml
+else echo "please copy .env.example to .env and enter your config"; exit 1
+fi
+docker build -t nwaku:latest -f docker/Dockerfile-nwaku docker
