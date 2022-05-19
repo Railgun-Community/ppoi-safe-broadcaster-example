0c6890dc512091ce3a1271f1b689cc117c0f5284
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-19 15:39:50 -0400

    working docker-compose.yml for nwaku + letsencrypt

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # docker build -t relayer:latest -f docker/Dockerfile .
-if [ -f docker/.env ]; then
-  echo "found config, good job"
-  export $(cat docker/.env | xargs)
-  envsubst < docker/config.toml.in > docker/config.toml
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
+if [ -f .env ]; then
+  if [ ! -f config.toml ]; then
+    export $(cat .env | xargs)
+    envsubst < config.toml.in > config.toml
+  fi
 else echo "please copy .env.example to .env and enter your config"; exit 1
 fi
-docker build -t nwaku:latest -f docker/Dockerfile-nwaku docker
+docker build -t nwaku:latest -f Dockerfile-nwaku .
