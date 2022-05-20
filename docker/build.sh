76e6feb5fddee600a7d388cfdbbca440938cc5b4
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-19 23:36:12 -0400

    initial docker stack

--- docker/build.sh
+++ docker/build.sh
@@ -2,10 +2,11 @@
 # docker build -t relayer:latest -f docker/Dockerfile .
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 if [ -f .env ]; then
-  if [ ! -f config.toml ]; then
+  if [ ! -f docker-stack.yml]; then
     export $(cat .env | xargs)
-    envsubst < config.toml.in > config.toml
-  fi
+    #envsubst < config.toml.in > config.toml
+    envsubst < docker-stack.yml.in > docker-stack.yml
+  #fi
 else echo "please copy .env.example to .env and enter your config"; exit 1
 fi
-docker build -t nwaku:latest -f Dockerfile-nwaku .
+#docker build -t nwaku:latest -f Dockerfile-nwaku .
