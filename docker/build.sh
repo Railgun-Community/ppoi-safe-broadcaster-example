30b51571862d373ba5d3d4f468d0b034c3ef0445
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-20 00:16:29 -0400

    nwaku uses NODEKEY secret

--- docker/build.sh
+++ docker/build.sh
@@ -2,11 +2,11 @@
 # docker build -t relayer:latest -f docker/Dockerfile .
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 if [ -f .env ]; then
-  if [ ! -f docker-stack.yml]; then
+  if [ ! -f docker-stack.yml ]; then
     export $(cat .env | xargs)
     #envsubst < config.toml.in > config.toml
     envsubst < docker-stack.yml.in > docker-stack.yml
-  #fi
+  fi
 else echo "please copy .env.example to .env and enter your config"; exit 1
 fi
 #docker build -t nwaku:latest -f Dockerfile-nwaku .
