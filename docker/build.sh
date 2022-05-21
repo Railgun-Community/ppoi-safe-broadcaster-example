7da069a7d2de04522b8fa6f5a8067cbf16fcb289
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-21 13:38:27 -0400

    fix nwaku volume config.toml issue

--- docker/build.sh
+++ docker/build.sh
@@ -10,5 +10,5 @@ else echo "please copy .env.example to .env and enter your config"; exit 1
 fi
 
 docker build -t relayer:latest -f relayer/Dockerfile ..
-docker build -t nwaku -f nwaku/Dockerfile nwaku
+docker build --no-cache -t nwaku:latest -f nwaku/Dockerfile nwaku
 echo "generated docker-stack.yml from .env and docker-stack.yml.in"
