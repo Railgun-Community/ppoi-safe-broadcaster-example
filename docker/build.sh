b938b223fadc617a1ac8159e94b6c4047821735e
Dylan Oliver <dylan.oliver@gmail.com>
2022-05-24 13:52:26 -0400

    create appdata/swag in build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -9,6 +9,8 @@ if [ -f .env ]; then
 else echo "please copy .env.example to .env and enter your config"; exit 1
 fi
 
+mkdir -p appdata/swag
+
 docker build -t relayer:latest -f relayer/Dockerfile ..
 docker build --no-cache -t nwaku:latest -f nwaku/Dockerfile nwaku
 echo "generated docker-stack.yml from .env and docker-stack.yml.in"
