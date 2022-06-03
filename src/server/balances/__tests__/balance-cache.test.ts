abb9b17ce7b056da09cc6fc70b684f804f92e15c
zibens <silas@Silass-MBP.localdomain>
2022-06-03 11:27:36 -0400

    update readme

--- src/server/balances/__tests__/balance-cache.test.ts
+++ src/server/balances/__tests__/balance-cache.test.ts
@@ -29,28 +29,28 @@ const shouldUpdateEthWalletBalance = (address: string) => {
   return shouldUpdateCachedGasTokenBalance(NetworkChainID.Ethereum, address);
 };
 
-describe('balance-cache', () => {
-  before(async () => {
+describe('balance-cache', () => { // name of the test suite
+  before(async () => { // before running any test - initLepton (the sdk), init the wallets, init network providers, and some helperfunrctions
     initLepton();
     await initWallets();
     initNetworkProviders();
     resetGasTokenBalanceCache();
-    createGasBalanceStub(BigNumber.from(5000));
+    createGasBalanceStub(BigNumber.from(5000)); // 
   });
 
-  afterEach(() => {
+  afterEach(() => { // 
     resetGasTokenBalanceCache();
   });
 
-  after(() => {
-    restoreGasBalanceStub();
+  after(() => { // resets tests after 
+    restoreGasBalanceStub(); 
   });
 
-  it('Should update gas token balance cache for all chains', async () => {
+  it('Should update gas token balance cache for all chains', async () => { // tests always start with it - 
     const activeWallets = getActiveWallets();
     const firstWalletAddress = activeWallets[0].address;
     await updateAllActiveWalletsGasTokenBalances(activeWallets);
-    expect(shouldUpdateEthWalletBalance(firstWalletAddress)).to.be.false;
+    expect(shouldUpdateEthWalletBalance(firstWalletAddress)).to.be.false; 
     const ethBalance = await getCachedGasTokenBalance(
       NetworkChainID.Ethereum,
       firstWalletAddress,
@@ -60,7 +60,7 @@ describe('balance-cache', () => {
       NetworkChainID.Ethereum,
       firstWalletAddress,
     );
-    expect(ropstenBalance.toString()).to.equal('5000');
+    expect(ropstenBalance.toString()).to.equal('5000'); // asert 
   });
 
   it('Should only refresh balances when necessary', async () => {
