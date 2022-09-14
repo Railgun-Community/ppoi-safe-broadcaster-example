e98c2e6679002e8bc7c2273ae41f83b3ba36134e
Dylan Oliver <dylan.oliver@gmail.com>
2022-09-14 03:30:19 +0000

    print multiaddr with result for reconnected peers

--- scripts/nwaku/reconnect_peers.mjs
+++ scripts/nwaku/reconnect_peers.mjs
@@ -5,7 +5,7 @@ const { log } = console;
 
 const reconnect = async (peer) => {
   const res = await rpcRequest('post_waku_v2_admin_v1_peers', [[peer.multiaddr]]);
-  log(res);
+  log(peer.multiaddr, res);
 }
 
 const { result, error } = await rpcRequest('get_waku_v2_admin_v1_peers', []);
