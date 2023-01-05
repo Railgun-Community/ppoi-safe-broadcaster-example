62d54a4ff01d5afec265557944ca5871db53b8bc
jrmeurer <jrmeurer@wisc.edu>
2023-01-04 17:33:49 -0700

    Remove alchemy provider

--- src/server/config/fallback-providers/421613-arbitrum-goerli.ts
+++ src/server/config/fallback-providers/421613-arbitrum-goerli.ts
@@ -4,12 +4,6 @@ const config: FallbackProviderJsonConfig = {
   chainId: 421613,
   providers: [
     {
-      provider:
-        'https://arb-goerli.g.alchemy.com/v2/mjJ4r5yqS3ouvBBKUbcAZGFYIrcyguVE',
-      priority: 2,
-      weight: 1,
-    },
-    {
       provider: 'https://goerli-rollup.arbitrum.io/rpc',
       priority: 3,
       weight: 1,
