9b58887ec50c6b5d60593e381eb9d9ce194d0f63
jrmeurer <jrmeurer@wisc.edu>
2023-01-10 10:52:35 -0700

    Update slippage for L2s to 20%

--- src/server/config/config-fees.ts
+++ src/server/config/config-fees.ts
@@ -5,7 +5,13 @@ export const feeConfigL1: FeeConfig = {
   profit: 0.01,
 };
 
+/**
+ * Default fee config for L2 networks.
+ * Add large slippage buffer because gas estimates change commonly between Client -> Relayer by around 5-10%.
+ * This gas estimation difference is only relevant on L2s.
+ * If gas doesn't spike to this degree, this buffer becomes profit.
+ */
 export const feeConfigL2: FeeConfig = {
-  slippageBuffer: 0.1,
-  profit: 0.05,
+  slippageBuffer: 0.2,
+  profit: 0.01,
 };
