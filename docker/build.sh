f169c320079e27c7b7e0cb41b08ddecbed86a3dd
Dylan Oliver <dylan.oliver@gmail.com>
2023-02-14 11:19:15 -0500

    docker/build.sh now fails loudly if either step fails, docker/run.sh prints stack service info

--- docker/build.sh
+++ docker/build.sh
@@ -12,12 +12,25 @@ if [ -f .env ]; then
   if [ ! -f docker-stack.yml ]; then
     export $(cat .env | xargs)
     envsubst < docker-stack.yml.in > docker-stack.yml
+    echo "generated docker-stack.yml from .env and docker-stack.yml.in"
   fi
 else echo "please copy .env.example to .env and enter your config"; exit 1
 fi
 
 mkdir -p appdata/swag
 
+# build relayer and exit 1 if it failed
 docker build -t relayer:latest -f relayer/Dockerfile ..
+if [ $? != 0 ]; then
+  echo "FAILED TO BUILD RELAYER IMAGE"
+  exit 1
+fi
+
+# build nwaku and exit 1 if it failed
 docker build --pull -t nwaku:latest -f nwaku/Dockerfile nwaku
-echo "generated docker-stack.yml from .env and docker-stack.yml.in"
+if [ $? != 0 ]; then
+  echo "FAILED TO BUILD RELAYER IMAGE"
+  exit 1
+fi
+
+echo "SUCCESS building relayer and nwaku images"
--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 docker stack deploy -c docker-stack.yml -c custom.yml relayer
+echo \n
+docker stack services relayer
