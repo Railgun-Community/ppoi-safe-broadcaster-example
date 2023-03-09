23965b182d4aa54f8dcd44441e66e6d3ce651500
Dylan Oliver <dylan.oliver@gmail.com>
2023-03-09 06:44:21 -0500

    remove bad omniatech rpc for arbitrum

--- src/server/config/fallback-providers/42161-arbitrum.ts
+++ src/server/config/fallback-providers/42161-arbitrum.ts
@@ -4,12 +4,6 @@ const config: FallbackProviderJsonConfig = {
   chainId: 42161,
   providers: [
     {
-      provider: 'https://endpoints.omniatech.io/v1/arbitrum/one/public',
-      priority: 2,
-      weight: 2,
-      stallTimeout: 2500,
-    },
-    {
       provider: 'https://arbitrum.blockpi.network/v1/rpc/public',
       priority: 3,
       weight: 1,
