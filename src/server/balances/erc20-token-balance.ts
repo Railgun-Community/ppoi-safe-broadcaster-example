7b05885a2ca7007323126c9013b410136226a9d4
zy0n <zy0n.barto@gmail.com>
2023-09-15 05:02:22 +0000

    add some promiseTimeouts to token-balance & gas validator

--- src/server/balances/erc20-token-balance.ts
+++ src/server/balances/erc20-token-balance.ts
@@ -4,6 +4,9 @@ import { logger } from '../../util/logger';
 import { getProviderForNetwork } from '../providers/active-network-providers';
 import { RelayerChain } from '../../models/chain-models';
 import { ABI_ERC20 } from '../abi/abi';
+import { promiseTimeout, throwErr } from '../../util/promise-utils';
+
+const BALANCE_TIMEOUT = 10 * 1000;
 
 export const getERC20TokenBalance = async (
   chain: RelayerChain,
@@ -13,7 +16,13 @@ export const getERC20TokenBalance = async (
   try {
     const provider = getProviderForNetwork(chain);
     const contract = new Contract(token.address, ABI_ERC20, provider);
-    return await contract.balanceOf(walletAddress);
+
+    const balance = await promiseTimeout(
+      contract.balanceOf(walletAddress),
+      BALANCE_TIMEOUT,
+    ).catch(throwErr);
+
+    return balance;
   } catch (err) {
     logger.warn(`Could not get gas token balance: ${err.message}`);
     return 0n;
