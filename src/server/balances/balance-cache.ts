3ff42455b1a3a55e89f06aa4a3692cd1cdfddff4
zy0n <zy0n.barto@gmail.com>
2023-09-15 04:35:40 +0000

    gas balance tweaks

--- src/server/balances/balance-cache.ts
+++ src/server/balances/balance-cache.ts
@@ -6,7 +6,7 @@ import { ActiveWallet } from '../../models/wallet-models';
 import { logger } from '../../util/logger';
 import { RelayerChain } from '../../models/chain-models';
 import { formatUnits } from 'ethers';
-import { isDefined } from '@railgun-community/shared-models';
+import { delay, isDefined } from '@railgun-community/shared-models';
 
 type CachedBalance = {
   balance: bigint;
@@ -23,9 +23,13 @@ export const resetGasTokenBalanceCache = () => {
 export const updateCachedGasTokenBalance = async (
   chain: RelayerChain,
   walletAddress: string,
+  retryCount = 0,
 ): Promise<void> => {
   const balance = await getGasTokenBalance(chain, walletAddress);
-  if (balance == null) {
+  if (!isDefined(balance)) {
+    if (retryCount < 3) {
+      return updateCachedGasTokenBalance(chain, walletAddress, retryCount + 1);
+    }
     return;
   }
   gasTokenBalanceCache[chain.type] ??= {};
@@ -72,12 +76,18 @@ export const getCachedGasTokenBalance = async (
   walletAddress: string,
 ): Promise<bigint> => {
   if (shouldUpdateCachedGasTokenBalance(chain, walletAddress)) {
-    await updateCachedGasTokenBalance(chain, walletAddress);
+    // first call needs to 'await' it.
+    if (!isDefined(gasTokenBalanceCache[chain.type][chain.id][walletAddress])) {
+      await updateCachedGasTokenBalance(chain, walletAddress);
+    } else {
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      updateCachedGasTokenBalance(chain, walletAddress); // dont wait for this promise, lazyLoad it instead.
+    }
   }
   gasTokenBalanceCache[chain.type] ??= {};
   gasTokenBalanceCache[chain.type][chain.id] ??= {};
   if (!isDefined(gasTokenBalanceCache[chain.type][chain.id][walletAddress])) {
-    throw new Error('No balance found');
+    return 0n; // return 0n instead
   }
   return gasTokenBalanceCache[chain.type][chain.id][walletAddress].balance;
 };
@@ -86,21 +96,19 @@ export const getActiveWalletGasTokenBalanceMapForChain = async (
   chain: RelayerChain,
   activeWallets: ActiveWallet[],
 ): Promise<MapType<bigint>> => {
-  const getBalancePromises: Promise<Optional<bigint>>[] = [];
-  activeWallets.forEach(({ address }) => {
-    getBalancePromises.push(
-      getCachedGasTokenBalance(chain, address).catch((err) => {
-        logger.error(err);
-        return undefined;
-      }),
-    );
-  });
-  const balances = await Promise.all(getBalancePromises);
+  const balancePromises: Promise<bigint>[] = [];
+  for (const { address } of activeWallets) {
+    balancePromises.push(getCachedGasTokenBalance(chain, address));
+    // eslint-disable-next-line no-await-in-loop
+    await delay(100);
+  }
+
+  const balanceResults = await Promise.all(balancePromises);
 
   const balanceMap: MapType<bigint> = {};
-  activeWallets.forEach((activeWallet, index) => {
-    const balance = balances[index];
-    if (balance != null) {
+  activeWallets.forEach(async (activeWallet, index) => {
+    const balance = balanceResults[index];
+    if (isDefined(balance)) {
       balanceMap[activeWallet.address] = balance;
     }
   });
