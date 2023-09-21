a8528acc69d3a93e7c709186ecbd39e0c7e39f17
zy0n <zy0n.barto@gmail.com>
2023-09-21 19:51:37 +0000

    setup helpers & readme upgrades, add swagless setup

--- scripts/nodekey.sh
+++ scripts/nodekey.sh
@@ -1,2 +1,20 @@
-#!/bin/sh
-tr -dc 'a-f0-9' < /dev/urandom | dd bs=1 count=64 2>/dev/null | xargs printf "0x%s"
+#!/bin/bash
+echo ""
+for ((i=1; i<=2; i++)); do
+    key=$(openssl rand -hex 32)
+    if grep -q "NODEKEY_${i}=" docker/.env; then
+      echo "NODEKEY_${i} already exists in .env."
+      if [ "$1" = "--set-secret" ]; then
+        echo "Setting Docker Secret for NODEKEY_${i}"
+        docker secret create NODEKEY_${i} - 0x${key}
+      else
+        echo "Not Setting Docker Secret values, please run again with --set-secret if you havent already. Or manually set them."
+      fi
+    else
+      echo "NODEKEY_${i}=0x${key}" >> docker/.env
+      echo "NODEKEY_${i} created with value: 0x${key}"
+    fi
+done
+echo ""
+
+
