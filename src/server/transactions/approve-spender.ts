9170ace92767a2c784830838c553983c39d49a4b
zy0n <zy0n.barto@gmail.com>
2023-09-21 03:27:07 +0000

    update approve spender logic

--- src/server/transactions/approve-spender.ts
+++ src/server/transactions/approve-spender.ts
@@ -16,32 +16,50 @@ import {
 
 const dbg = debug('relayer:approvals');
 
+export const MAX_UINT_ALLOWANCE = 2n ** 256n - 1n;
+
+export const cachedApprovedTokens: string[] = [];
+
 export const generateApprovalTransactions = async (
   erc20Amounts: ERC20Amount[],
   chain: RelayerChain,
+  sender: string,
 ): Promise<ContractTransaction[]> => {
-  const populatedTransactions: Optional<ContractTransaction>[] =
-    await Promise.all(
-      erc20Amounts.map(async (erc20Amount) => {
-        try {
-          const provider = getProviderForNetwork(chain);
-          const contract = new Contract(
-            erc20Amount.tokenAddress,
-            ABI_ERC20,
-            provider,
-          );
-          const approvalAmount = erc20Amount.amount;
-          const spender = zeroXExchangeProxyContractAddress(chain);
-          return await contract.approve.populateTransaction(
-            spender,
-            approvalAmount,
-          );
-        } catch (err) {
-          dbg(`Could not populate transaction for some token: ${err.message}`);
-          return undefined;
-        }
-      }),
-    );
+  const populatedTransactions: Optional<ContractTransaction>[] = [];
+
+  for (const tokenAmount of erc20Amounts) {
+    try {
+      const provider = getProviderForNetwork(chain);
+      const contract = new Contract(
+        tokenAmount.tokenAddress,
+        ABI_ERC20,
+        provider,
+      );
+
+      const spender = zeroXExchangeProxyContractAddress(chain);
+
+      // eslint-disable-next-line no-await-in-loop
+      const allowance: bigint = await contract.allowance(sender, spender);
+      if (allowance >= tokenAmount.amount) {
+        dbg('Enough Allowance granted already');
+        continue;
+      }
+      dbg('We dont have enough allowance, set it max');
+      // approve for infinite approval
+      dbg(allowance);
+      dbg(MAX_UINT_ALLOWANCE);
+
+      // eslint-disable-next-line no-await-in-loop
+      const populatedTransaction = await contract.approve.populateTransaction(
+        spender,
+        MAX_UINT_ALLOWANCE,
+      );
+      populatedTransaction.from = sender;
+      populatedTransactions.push(populatedTransaction);
+    } catch (err: any) {
+      dbg(`Could not populate transaction for some token: ${err.message}`);
+    }
+  }
 
   return removeUndefineds(populatedTransactions);
 };
@@ -54,34 +72,41 @@ export const approveZeroX = async (
   const populatedApprovalTXs = await generateApprovalTransactions(
     erc20Amounts,
     chain,
+    activeWallet.address,
   );
-  const TransactionResponses: TransactionResponse[] = await Promise.all(
-    populatedApprovalTXs.map(async (populatedApproval) => {
-      const network = networkForChain(chain);
-      if (!network) {
-        throw new Error(
-          `Unsupported network for chain ${chain.type}:${chain.id}`,
-        );
-      }
-      const sendWithPublicWallet = true;
-      const evmGasType = getEVMGasTypeForTransaction(
-        network.name,
-        sendWithPublicWallet,
+  const TransactionResponses: TransactionResponse[] = [];
+  // switch this to async
+  for (const populatedApproval of populatedApprovalTXs) {
+    const network = networkForChain(chain);
+    if (!network) {
+      throw new Error(
+        `Unsupported network for chain ${chain.type}:${chain.id}`,
       );
-
-      const gasDetails = await getEstimateGasDetailsPublic(
-        chain,
-        evmGasType,
-        populatedApproval,
-      );
-      const txResponse = await executeTransaction(
-        chain,
-        populatedApproval,
-        gasDetails,
-        activeWallet,
-      );
-      return txResponse;
-    }),
-  );
+    }
+    const sendWithPublicWallet = true;
+    const evmGasType = getEVMGasTypeForTransaction(
+      network.name,
+      sendWithPublicWallet,
+    );
+    // eslint-disable-next-line no-await-in-loop
+    const gasDetails = await getEstimateGasDetailsPublic(
+      chain,
+      evmGasType,
+      populatedApproval,
+    );
+    // eslint-disable-next-line no-await-in-loop
+    const txResponse = await executeTransaction(
+      chain,
+      populatedApproval,
+      gasDetails,
+      activeWallet,
+      undefined, // overrideNonce
+      false, // setAvailability
+      false, // setTxCached
+    );
+    // return txResponse;
+    TransactionResponses.push(txResponse);
+  }
+  await Promise.all(TransactionResponses);
   return removeUndefineds(TransactionResponses);
 };
