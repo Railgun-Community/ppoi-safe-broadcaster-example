1a4fa681c5c0305ad5f5d4c347119d0c1718bb3f
zy0n <zy0n.barto@gmail.com>
2023-10-06 01:01:46 +0000

    switch default eth providers around

--- src/server/config/fallback-providers/1-ethereum.ts
+++ src/server/config/fallback-providers/1-ethereum.ts
@@ -4,14 +4,14 @@ const config: FallbackProviderJsonConfig = {
   chainId: 1,
   providers: [
     {
-      provider: 'https://rpc.ankr.com/eth',
+      provider: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
       priority: 2,
       weight: 2,
       maxLogsPerBatch: 1, // Supports up to 10, but at 1 ethers handles getLogs differently, and this seems to be more stable.
       stallTimeout: 2500,
     },
     {
-      provider: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
+      provider: 'https://rpc.ankr.com/eth',
       priority: 3,
       weight: 1,
     },
