624a914d6f268806aa7b0ac8fbf26bd94b33daf5
zy0n <zy0n.barto@gmail.com>
2024-01-20 03:31:13 +0000

    add dust protection to public token watch

--- src/server/wallets/wallet-top-up-poller.ts
+++ src/server/wallets/wallet-top-up-poller.ts
@@ -11,7 +11,11 @@ import { RelayerChain } from '../../models/chain-models';
 import { removeUndefineds } from '../../util/utils';
 import debug from 'debug';
 import { getActiveWalletGasTokenBalanceMapForChain } from '../balances/balance-cache';
-import { TXIDVersion, isDefined } from '@railgun-community/shared-models';
+import { isDefined, TXIDVersion } from '@railgun-community/shared-models';
+import { getPublicERC20AmountsBeforeUnwrap } from '../balances/top-up-balance';
+import { lookUpCachedTokenPrice } from '../tokens/token-price-cache';
+import { tokenForAddress } from '../tokens/network-tokens';
+import { formatUnits, parseUnits } from 'ethers';
 
 const dbg = debug('relayer:top-up-poller');
 
@@ -35,7 +39,7 @@ const pollTopUp = async () => {
       if (isDefined(walletToTopUp)) {
         walletFound = true;
         logger.warn(
-          `We have a wallet to top up | address: ${walletToTopUp.address} on chain:${chain.id}`,
+          `We have a wallet to top up! ${walletToTopUp.address} chain:${chain.id}`,
         );
         const currentTXIDVersion = TXIDVersion.V2_PoseidonMerkle; // Switch this to V3 when balances migrated after release.
         // eslint-disable-next-line no-await-in-loop
@@ -83,7 +87,7 @@ export const initTopUpPoller = async () => {
     return;
   }
   // prevent this from starting for 10 mins
-  await delay(10 * 60 * 1000);
+  await delay(2 * 60 * 1000);
 
   // eslint-disable-next-line @typescript-eslint/no-floating-promises
   pollTopUp();
@@ -98,7 +102,46 @@ export const getTopUpWallet = async (
     chain,
     activeWallets,
   );
+  for (const activeWallet of activeWallets) {
+    // eslint-disable-next-line no-await-in-loop
+    const unwrappedTokensWaiting = await getPublicERC20AmountsBeforeUnwrap(
+      activeWallet,
+      chain,
+      true, // force scan here to prevent unecessary additional unshielding.
+    );
+    if (unwrappedTokensWaiting.length > 0) {
+      let totalTokenValue = 0;
+      let tokenAmountValueThreshold: Optional<number>;
+      for (const unshieldToken of unwrappedTokensWaiting) {
+        const tokenPrice = lookUpCachedTokenPrice(chain, unshieldToken.tokenAddress);
+        const token = tokenForAddress(chain, unshieldToken.tokenAddress);
+        const tokenAmountReadable = formatUnits(unshieldToken.amount, token.decimals);
+        const tokenAmountValue = parseFloat(tokenAmountReadable) * tokenPrice;
+        totalTokenValue += tokenAmountValue;
+        if (!isDefined(tokenAmountValueThreshold)) {
+          const { gasToken } = configNetworks[chain.type][chain.id];
+          const gasTokenPrice = lookUpCachedTokenPrice(chain, gasToken.wrappedAddress);
+          const { swapThresholdIntoGasToken } = configNetworks[chain.type][chain.id].topUp;
+          const gasTokenAmountReadable = formatUnits(swapThresholdIntoGasToken, gasToken.decimals);
+          const swapThresholdIntoGasTokenValue = parseFloat(gasTokenAmountReadable) * gasTokenPrice;
+          tokenAmountValueThreshold = swapThresholdIntoGasTokenValue / 3;
+        }
 
+        logger.warn(`Token ${unshieldToken.tokenAddress} on ${activeWallet.address} on chain ${chain.type}:${chain.id}.`)
+        logger.warn(`Token Price: ${tokenPrice}`);
+        logger.warn(`Token Amount Readable: ${tokenAmountReadable}`);
+        logger.warn(`Token Amount Value: ${tokenAmountValue}`);
+        logger.warn(`Token Value Threshold: ${tokenAmountValueThreshold}`);
+      }
+      if (isDefined(tokenAmountValueThreshold)) {
+        if (totalTokenValue > tokenAmountValueThreshold) {
+          logger.warn(`Tokens awaiting ${unwrappedTokensWaiting.length} | ${activeWallet.address} on chain ${chain.type}:${chain.id}.`);
+          return activeWallet
+        }
+      }
+    }
+    logger.warn(`We have ${unwrappedTokensWaiting.length} tokens awaiting on ${activeWallet.address} on chain ${chain.type}:${chain.id}.`)
+  }
   // asending sort
   const topUpWallets: Optional<ActiveWallet>[] = activeWallets
     .filter((wallet) => {
