7128da994a1ef10c7ca788b5b424e7f635756761
zy0n <zy0n.barto@gmail.com>
2024-03-16 01:41:52 +0000

    update helper scripts

--- scripts/nwaku/peers.sh
+++ scripts/nwaku/peers.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 curl -X GET "http://localhost:8546/admin/v1/peers" -H "accept: application/json" | jq
+curl -X GET "http://localhost:8547/admin/v1/peers" -H "accept: application/json" | jq
--- scripts/nwaku/version.sh
+++ scripts/nwaku/version.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-curl -X GET "http://localhost:8645/debug/v1/version" -H "accept: text/plain"
+curl -X GET "http://localhost:8546/debug/v1/version" -H "accept: text/plain"
+curl -X GET "http://localhost:8547/debug/v1/version" -H "accept: text/plain"
 
