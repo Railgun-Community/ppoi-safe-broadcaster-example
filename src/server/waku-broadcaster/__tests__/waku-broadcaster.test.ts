6c5939824327b8cf58dad656f246c8969f9bb716
zy0n <zy0n.barto@gmail.com>
2024-10-07 10:49:22 -0400

    fix test

--- src/server/waku-broadcaster/__tests__/waku-broadcaster.test.ts
+++ src/server/waku-broadcaster/__tests__/waku-broadcaster.test.ts
@@ -290,7 +290,7 @@ describe('waku-broadcaster', () => {
     await wakuBroadcaster?.handleMessage(relayMessage);
 
     // After transact-response sent.
-    expect(clientHTTPStub.callCount).to.equal(20);
+    expect(clientHTTPStub.callCount).to.equal(10);
     const postCall = clientHTTPStub.getCall(0);
     expect(postCall.args[0]).to.equal(WakuRequestMethods.PublishMessage);
     const rpcArgs = postCall.args[1];
