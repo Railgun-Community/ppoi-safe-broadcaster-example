0d04295cf970f70eb14639eab1ba9b220bf784ea
zy0n <zy0n.barto@gmail.com>
2025-05-16 14:19:59 -0400

    feat: enhance transaction validation by including chain parameter in createValidTransaction

--- src/server/transactions/transaction-validator.ts
+++ src/server/transactions/transaction-validator.ts
@@ -1,7 +1,11 @@
-import { ContractTransaction, getAddress, isHexString } from 'ethers';
+import { ContractTransaction, getAddress, Interface, isHexString, type InterfaceAbi, type TransactionDescription } from 'ethers';
 import { logger } from '../../util/logger';
+import { ABI_RELAY_ADAPT } from '../abi/abi';
+import type { BroadcasterChain } from '../../models/chain-models';
+import configNetworks from '../config/config-networks';
 
 export const createValidTransaction = (
+  chain: BroadcasterChain,
   to: string,
   data: string,
   value?: bigint,
@@ -10,14 +14,75 @@ export const createValidTransaction = (
     if (!isHexString(data)) {
       throw new Error('Invalid data field.');
     }
+    const isProxyCall = checkIsProxyAddress(chain, to);
+    const isRelayCall =  checkIsRelayAdaptAddress(chain, to);
+    const validatedTo = isProxyCall || isRelayCall;
+
+    if(!validatedTo){
+      throw new Error("Invalid to address. Must be a known contract.")
+    }
+
+    if(isRelayCall){
+      const decoded = decodeData(data, ABI_RELAY_ADAPT);
+      if(decoded){
+        const needsValidation = isCallToRelay(decoded);
+        if(needsValidation){
+          const validatedData = validateRelayCallData(decoded);
+          if(!validatedData){
+            throw new Error("Invalid Transaction Call.")
+          }
+        }
+      }
+    }
     const validTransaction: ContractTransaction = {
       to: getAddress(to),
       data,
       value,
     };
+
     return validTransaction;
   } catch (err) {
     logger.error(err);
     throw new Error('Could not create valid transaction object.');
   }
 };
+
+const decodeData = (data: string, abi: InterfaceAbi) => {
+  const iface = new Interface(abi);
+  try {
+    const decoded = iface.parseTransaction({ data });
+    return decoded;
+  } catch {
+    return null;
+  }
+};
+
+const isCallToRelay = (decoded: TransactionDescription | null) =>{
+  if (!decoded) return false;
+  if (decoded.name !== "relay") return false;
+  return true
+}
+
+const validateRelayCallData = (decoded: TransactionDescription) => {
+  const invalid: boolean = decoded.args[1][1];
+  return !invalid;
+};
+
+const checkIsProxyAddress = (
+  chain: BroadcasterChain,
+  address: string,
+) =>{
+  const { proxyContract } =
+    configNetworks[chain.type][chain.id];
+  return address.toLowerCase() === proxyContract.toLowerCase()
+
+}
+
+const checkIsRelayAdaptAddress = (
+  chain: BroadcasterChain,
+  address: string,
+) =>{
+  const { relayAdaptContract } =
+    configNetworks[chain.type][chain.id];
+  return address.toLowerCase() === relayAdaptContract.toLowerCase()
+}
