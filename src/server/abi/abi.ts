010173c3363ab0bb2cecd70f8f4bf51e3df19bf7
zy0n <zy0n.barto@gmail.com>
2025-05-16 14:19:43 -0400

    feat: add relay ABI and export in abi.ts

--- src/server/abi/abi.ts
+++ src/server/abi/abi.ts
@@ -1,4 +1,5 @@
 import ABI_ERC20 from './json/erc20.json';
 import ABI_PERMIT2 from './json/permit2.json';
+import ABI_RELAY_ADAPT from './json/relay.json';
 
-export { ABI_ERC20, ABI_PERMIT2 };
+export { ABI_ERC20, ABI_PERMIT2, ABI_RELAY_ADAPT };
