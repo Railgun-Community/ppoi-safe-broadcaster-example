7ee0c5feca3a1470bae6800bc6f53a0905f622f8
zy0n <zy0n.barto@gmail.com>
2025-07-28 19:23:44 -0400

    fix: remove broen provider

--- src/server/config/fallback-providers/1-ethereum.ts
+++ src/server/config/fallback-providers/1-ethereum.ts
@@ -4,7 +4,7 @@ const config: FallbackProviderJsonConfig = {
   chainId: 1,
   providers: [
     {
-      provider: 'https://mainnet.infura.io/v3/84842078b09946638c03157f83405213',
+      provider: 'https://eth.llamarpc.com',
       priority: 2,
       weight: 2,
       maxLogsPerBatch: 1, // Supports up to 10, but at 1 ethers handles getLogs differently, and this seems to be more stable.
